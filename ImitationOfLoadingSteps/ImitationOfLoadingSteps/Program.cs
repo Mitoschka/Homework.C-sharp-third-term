f2ff2868264bb18aabee068fbb39c7dd337d8a1e
Admin <Michael-200>
2020-09-02 17:34:44 +0600

    Update Program.cs
    > Add ShowSplash();
    > Add RequestLicense();
    > Add CheckUpdate();
    > Add DownloadUpdate();
    > Add DisplayScreen();
    > Add HideSplash();
    > Add SetupMenus();
    > Change Main

--- ImitationOfLoadingSteps/ImitationOfLoadingSteps/Program.cs
+++ ImitationOfLoadingSteps/ImitationOfLoadingSteps/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Threading.Tasks;
 
+/// <summary>
+/// Global namespace.
+/// </summary>
 namespace ImitationOfLoadingSteps
 {
+    /// <summary>
+    /// Program launch
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Launches programs
+        /// </summary>
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var splash = Task.Run(() => ShowSplash());
+            var errorOfShowSplash = splash.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var license = splash.ContinueWith(ant => RequestLicense(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfRequestLicense = license.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var setupMenu = license.ContinueWith(ant => SetupMenus(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfSetupMenu = setupMenu.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var update = splash.ContinueWith(ant => CheckUpdate(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfCheckUpdate = update.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var download = update.ContinueWith(ant => DownloadUpdate(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfDownloadUpdate = download.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var display = Task.Factory.ContinueWhenAll(new[] { setupMenu, download }, tasks => DisplayScreen())
+                .ContinueWith(ant => HideSplash());
+            display.Wait();
+            Console.WriteLine(display.IsCompleted);
+        }
+
+        /// <summary>
+        /// Show splash
+        /// </summary>
+        private static void ShowSplash()
+        {
+            Console.WriteLine("Show Splash");
+        }
+
+        /// <summary>
+        /// Request license
+        /// </summary>
+        private static void RequestLicense()
+        {
+            Console.WriteLine("Request License");
+        }
+
+        /// <summary>
+        /// Check update
+        /// </summary>
+        private static void CheckUpdate()
+        {
+            Console.WriteLine("Check for Update");
+        }
+
+        /// <summary>
+        /// Download update
+        /// </summary>
+        private static void DownloadUpdate()
+        {
+            Console.WriteLine("Download Update");
+        }
+
+        /// <summary>
+        /// Show display screen
+        /// </summary>
+        private static void DisplayScreen()
+        {
+            Console.WriteLine("Display Welcome Screen");
+        }
+
+        /// <summary>
+        /// Show hide splash
+        /// </summary>
+        private static void HideSplash()
+        {
+            Console.WriteLine("Hide Splash");
+        }
+
+        /// <summary>
+        /// Show setup menus
+        /// </summary>
+        private static void SetupMenus()
+        {
+            Console.WriteLine("Setup Menus");
         }
     }
 }
