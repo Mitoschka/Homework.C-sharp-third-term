8809c89071af2b9ca911011e936b9a5b8a7b0124
Admin <Michael-200>
2020-09-03 21:21:50 +0600

    Add Creater of randoms exceptions in Program.cs

--- ImitationOfLoadingSteps/ImitationOfLoadingSteps/Program.cs
+++ ImitationOfLoadingSteps/ImitationOfLoadingSteps/Program.cs
@@ -11,32 +11,27 @@ namespace ImitationOfLoadingSteps
     /// </summary>
     class Program
     {
+        private static String result = "Program completed successfully";
+
         /// <summary>
         /// Launches programs
         /// </summary>
-        static void Main()
+        private static void Main()
         {
-            try
-            {
-                var splash = Task.Run(() => ShowSplash());
-                var errorOfShowSplash = splash.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
-                var license = splash.ContinueWith(ant => RequestLicense(), TaskContinuationOptions.NotOnCanceled);
-                var errorOfRequestLicense = license.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
-                var setupMenu = license.ContinueWith(ant => SetupMenus(), TaskContinuationOptions.NotOnCanceled);
-                var errorOfSetupMenu = setupMenu.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
-                var update = splash.ContinueWith(ant => CheckUpdate(), TaskContinuationOptions.NotOnCanceled);
-                var errorOfCheckUpdate = update.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
-                var download = update.ContinueWith(ant => DownloadUpdate(), TaskContinuationOptions.NotOnCanceled);
-                var errorOfDownloadUpdate = download.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
-                var display = Task.Factory.ContinueWhenAll(new[] { setupMenu, download }, tasks => DisplayScreen())
-                    .ContinueWith(ant => HideSplash());
-                display.Wait();
-                Console.WriteLine(display.IsCompleted);
-            }
-            catch(MyException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            var splash = Task.Run(() => ShowSplash());
+            var errorOfShowSplash = splash.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var license = splash.ContinueWith(ant => RequestLicense(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfRequestLicense = license.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var setupMenu = license.ContinueWith(ant => SetupMenus(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfSetupMenu = setupMenu.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var update = splash.ContinueWith(ant => CheckUpdate(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfCheckUpdate = update.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var download = update.ContinueWith(ant => DownloadUpdate(), TaskContinuationOptions.NotOnCanceled);
+            var errorOfDownloadUpdate = download.ContinueWith(ant => Console.WriteLine(ant.Exception), TaskContinuationOptions.OnlyOnFaulted);
+            var display = Task.Factory.ContinueWhenAll(new[] { setupMenu, download }, tasks => DisplayScreen())
+                .ContinueWith(ant => HideSplash());
+            display.Wait();
+            Console.WriteLine($"\n\n\n*{result}*");
         }
 
         /// <summary>
@@ -44,7 +39,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void ShowSplash()
         {
-            Console.WriteLine("Show Splash");
+            String name = "Show splash";
+            Console.Write(name);
+            RandomException(name);
         }
 
         /// <summary>
@@ -52,7 +49,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void RequestLicense()
         {
-            Console.WriteLine("Request License");
+            String name = "Request License";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
         }
 
         /// <summary>
@@ -60,7 +59,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void CheckUpdate()
         {
-            Console.WriteLine("Check for Update");
+            String name = "Check for Update";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
         }
 
         /// <summary>
@@ -68,7 +69,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void DownloadUpdate()
         {
-            Console.WriteLine("Download Update");
+            String name = "Download Update";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
         }
 
         /// <summary>
@@ -76,7 +79,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void DisplayScreen()
         {
-            Console.WriteLine("Display Welcome Screen");
+            String name = "Display Welcome Screen";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
         }
 
         /// <summary>
@@ -84,7 +89,9 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void HideSplash()
         {
-            Console.WriteLine("Hide Splash");
+            String name = "Hide Splash";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
         }
 
         /// <summary>
@@ -92,7 +99,31 @@ namespace ImitationOfLoadingSteps
         /// </summary>
         private static void SetupMenus()
         {
-            Console.WriteLine("Setup Menus");
+            String name = "Setup Menus";
+            Console.Write($"\n\n{name}");
+            RandomException(name);
+        }
+
+        /// <summary>
+        /// Create random exception
+        /// </summary>
+        /// <param name="name"> The name of the method in which the error occurred </param>
+        private static void RandomException(String name)
+        {
+            try
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(0, 10);
+                if ((value % 5) == 0)
+                {
+                    throw new MyException(name);
+                }
+            }
+            catch (MyException e)
+            {
+                Console.Write($"    --> Error with : {e.Message}");
+                result = "Program not completed";
+            }
         }
     }
 }
